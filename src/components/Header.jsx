452bb8873839e2c8a7d046a9b7921b42ec4e8a79
jasur <fake49735@gmail.com>
2023-10-28 14:30:11 +0500

    Header Icons is changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { style } from '../assets/constant/style'
 import pcImg from '../assets/img/PC.png'
+import { Icon } from '@iconify/react';
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Header() {
   return (
-    <header className='pt-[100px] md:pt-[140px] pb-[50px] md:pb-[100px] bg-headerBackground bg-center bg-no-repeat bg-cover px-[20px] md:px-[50px] lg:px-[100px]'>
+    <header className='pt-[100px] md:pt-[140px] pb-[50px] md:pb-[100px] bg-headerBackground bg-center bg-no-repeat bg-cover md:px-[50px] lg:px-[100px]'>
       <div className='container m-auto'>
 
-        <div className='flex gap-3 md:gap-5'>
+        <div className='px-[20px] md:px-[0px] flex gap-3 md:gap-5'>
 
           <div className='flex flex-col justify-center items-center mt-5'>
             <div className='w-4 h-4 md:w-5 md:h-5 rounded-full bg-[#915EFF]' />
-            <div className='w-1 h-[100px] md:h-[210px] lg:h-[230px] violet-gradient' />
+            <div className='w-1 h-[130px] md:h-[210px] lg:h-[230px] violet-gradient' />
           </div>
 
           <div className='mt-[10px] lg:mt-[0px]'>
@@ -19,43 +21,55 @@ export default function Header() {
             <h1 className={`${style.heroHeadText} text-white`}> Hi, I'm <span className='text-[#915EFF]'>Jasur</span> </h1>
             <p className={`${style.heroSubText} text-white-100`}> Frontend Developer </p>
 
-            <ul className='md:flex justify-center items-center gap-[20px] px-[12px] py-[12px] bg-[#101624] mt-[15px] rounded-[50px] border-[1px] border-[#232935] hidden'>
+            <div className='md:flex justify-center items-center gap-[20px] px-[12px] py-[12px] bg-[#101624] mt-[15px] rounded-[50px] border-[1px] border-[#232935] hidden'>
 
-              <li>
-                  <a href="#!" className={`${style.aHrefLink}`}>
-                    <i class={`${style.svgAlink} fa-brands fa-linkedin`}></i>
-                    <span className={`${style.spanLink}`}>Linkedln</span>
-                  </a>
-              </li>
+              <a href="#!" className={`${style.aHrefLink}`}>
+                <Icon icon="skill-icons:linkedin" color="blue" width="25" />
+                <span className={`${style.spanLink}`}>Linkedln</span>
+              </a>
 
-              <li>
-                  <a href="#!" className={`${style.aHrefLink}`}>
-                    <i class={`${style.svgAlink} fa-brands fa-square-github`}></i>
-                    <span className={`${style.spanLink} hidden`}>GitHub</span>
-                  </a>
-              </li>
+              <a href="#!" className={`${style.aHrefLink}`}>
+                <Icon icon="fa:github-square" color="white" width="25" />
+                <span className={`${style.spanLink}`}>GitHub</span>
+              </a>
 
-              <li>
-                  <a href="#!" className={`${style.aHrefLink}`}>
-                    <i class={`${style.svgAlink} fa-brands fa-telegram`}></i>
-                    <span className={`${style.spanLink} hidden`}>Telegram</span>
-                  </a>
-              </li>
+              <a href="#!" className={`${style.aHrefLink}`}>
+                <Icon icon="logos:telegram" color="blue" width="25" />
+                <span className={`${style.spanLink}`}>Telegram</span>
+              </a>
 
-              <li>
-                  <a href="#!" className={`${style.aHrefLink}`}>
-                    <i class={`${style.svgAlink} fa-solid fa-envelope`}></i>
-                    <span className={`${style.spanLink} hidden`}>Email</span>
-                  </a>
-              </li>
+              <a href="#!" className={`${style.aHrefLink}`}>
+                <Icon icon="logos:google-gmail" color="blue" width="25" />
+                <span className={`${style.spanLink}`}>Email</span>
+              </a>
 
-            </ul>
+            </div>
 
           </div>
 
         </div>
 
-        <img src={pcImg} className='m-auto w-full mt-[40px] md:mt-[75px] xl:w-4/5 2xl:w-3/4' alt="" />
+        <img src={pcImg} className='px-[5px] md:px-[0px] m-auto w-full mt-[40px] md:mt-[75px] xl:w-4/5 2xl:w-3/4' alt="" />
+
+        <ul className='flex justify-center items-center gap-[30px] mt-[50px] md:hidden'>
+
+          <a href="#!">
+            <Icon icon="skill-icons:linkedin" color="blue" width="30" />
+          </a>
+
+          <a href="#!">
+            <Icon icon="fa:github-square" color="white" width="30" />
+          </a>
+
+          <a href="#!">
+            <Icon icon="logos:telegram" color="blue" width="30" />
+          </a>
+
+          <a href="#!">
+            <Icon icon="logos:google-gmail" color="blue" width="30" />
+          </a>
+
+        </ul>
 
       </div>
     </header>
