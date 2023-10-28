5d517343e25b05a8979556a8297607d6f195eb29
jasur <fake49735@gmail.com>
2023-10-28 12:22:08 +0500

    Header Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,30 +4,58 @@ import pcImg from '../assets/img/PC.png'
 
 export default function Header() {
   return (
-    <header className='pt-[140px] pb-[100px] bg-headerBackground bg-center bg-no-repeat bg-cover lg:px-[100px]'>
+    <header className='pt-[100px] md:pt-[140px] pb-[50px] md:pb-[100px] bg-headerBackground bg-center bg-no-repeat bg-cover px-[20px] md:px-[50px] lg:px-[100px]'>
       <div className='container m-auto'>
 
-        <div className='flex gap-5'>
+        <div className='flex gap-3 md:gap-5'>
 
           <div className='flex flex-col justify-center items-center mt-5'>
-            <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-            <div className='w-1 h-[200px] violet-gradient' />
+            <div className='w-4 h-4 md:w-5 md:h-5 rounded-full bg-[#915EFF]' />
+            <div className='w-1 h-[100px] md:h-[210px] lg:h-[230px] violet-gradient' />
           </div>
 
-          <div>
-            
+          <div className='mt-[10px] lg:mt-[0px]'>
+
             <h1 className={`${style.heroHeadText} text-white`}> Hi, I'm <span className='text-[#915EFF]'>Jasur</span> </h1>
-            <p className={`${style.heroSubText} my-2 text-white-100`}> Frontend Developer </p>
+            <p className={`${style.heroSubText} text-white-100`}> Frontend Developer </p>
 
-            <div className='bg-[#101624] w-[200px] p-[20px] rounded-[50px] border-[1px] border-[#23293]'>
+            <ul className='md:flex justify-center items-center gap-[20px] px-[12px] py-[12px] bg-[#101624] mt-[15px] rounded-[50px] border-[1px] border-[#232935] hidden'>
 
-            </div>
+              <li>
+                  <a href="#!" className={`${style.aHrefLink}`}>
+                    <i class={`${style.svgAlink} fa-brands fa-linkedin`}></i>
+                    <span className={`${style.spanLink}`}>Linkedln</span>
+                  </a>
+              </li>
+
+              <li>
+                  <a href="#!" className={`${style.aHrefLink}`}>
+                    <i class={`${style.svgAlink} fa-brands fa-square-github`}></i>
+                    <span className={`${style.spanLink} hidden`}>GitHub</span>
+                  </a>
+              </li>
+
+              <li>
+                  <a href="#!" className={`${style.aHrefLink}`}>
+                    <i class={`${style.svgAlink} fa-brands fa-telegram`}></i>
+                    <span className={`${style.spanLink} hidden`}>Telegram</span>
+                  </a>
+              </li>
+
+              <li>
+                  <a href="#!" className={`${style.aHrefLink}`}>
+                    <i class={`${style.svgAlink} fa-solid fa-envelope`}></i>
+                    <span className={`${style.spanLink} hidden`}>Email</span>
+                  </a>
+              </li>
+
+            </ul>
 
           </div>
 
         </div>
 
-        <img src={pcImg} className='m-auto w-full mt-[50px] xl:w-4/5 2xl:w-3/4' alt="" />
+        <img src={pcImg} className='m-auto w-full mt-[40px] md:mt-[75px] xl:w-4/5 2xl:w-3/4' alt="" />
 
       </div>
     </header>
