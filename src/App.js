c1c7ce70887b64449ab6f9a7465d06db669cb2f9
jasur <fake49735@gmail.com>
2023-11-20 13:57:52 +0500

    Checking

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Main from './components/Main';
-import Section from './components/Section';
+// import Section from './components/Section';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Navbar/>
       <Header/>
       <Main/>
-      <Section/>
+      {/* <Section/> */}
     </div>
   );
 }
