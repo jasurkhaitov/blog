4182bfc4d518a4418b27e6cf1dd6dc22d2915af2
jasur <fake49735@gmail.com>
2024-06-05 23:28:11 +0500

    completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div className='bg-[#0a101e] opacity-[0.99]'>
-        <p className='text-center py-[20px] text-[16px] font-medium text-[#aaa6c3]'>© 2023. All right reserved.</p>
+        <p className='text-center py-[20px] text-[16px] font-medium text-[#aaa6c3]'>© 2023 JasX. All right reserved.</p>
     </div>
   )
 }
