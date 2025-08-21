6e5870c13247e8e41acd9917b70098acbab4bcf1
jasur <fake49735@gmail.com>
2025-08-22 02:52:24 +0500

    vite updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,37 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	build: {
-		sourcemap: false,
-		chunkSizeWarningLimit: 1000,
-		outDir: 'dist',
-		target: 'esnext',
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes('node_modules')) {
-						if (id.includes('react')) {
-							return 'react'
-						}
-						if (id.includes('lodash')) {
-							return 'lodash'
-						}
-						if (id.includes('chart.js')) {
-							return 'chartjs'
-						}
-						return 'vendor'
-					}
-				},
-				entryFileNames: 'assets/[name].[hash].js',
-				chunkFileNames: 'assets/[name].[hash].js',
-				assetFileNames: 'assets/[name].[hash].[ext]',
-			},
-		},
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
