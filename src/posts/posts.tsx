b21d553339ec7097cb8946b4b437e2e6a6015427
jasur <fake49735@gmail.com>
2025-08-28 22:12:14 +0500

    posts removed

--- src/posts/posts.tsx
+++ src/posts/posts.tsx
@@ -1,90 +1,90 @@
-import Img from "../assets/example.webp"
+// import Img from "../assets/example.webp"/*  */
 
 export const listOfPost = [
-  {
-    id: 1,
-    img: Img,
-    hashtag: [
-      {
-        name: '#js',
-        color: 'text-yellow-600',
-      },
-      {
-        name: '#ts',
-        color: 'text-blue-700',
-      },
-      {
-        name: '#html',
-        color: 'text-orange-600',
-      },
-      {
-        name: '#css',
-        color: ' text-indigo-600',
-      }
-    ],
-    name: "Hello World",
-    slug: "hello-world",
-    date: "2025-08-14 18:02",
-    file: () => import("./hello-world.md?raw"),
-    pinned: false,
-    lang: 'uz'
-  },
-  {
-    id: 2,
-    img: Img,
-    hashtag: [
-      {
-        name: '#js',
-        color: ' text-yellow-600',
-      },
-      {
-        name: '#ts',
-        color: ' text-blue-600',
-      },
-      {
-        name: '#react',
-        color: ' text-cyan-600',
-      },
-      {
-        name: '#angular',
-        color: ' text-red-600',
-      },
-    ],
-    name: "Nimadur",
-    slug: "xayr-world",
-    date: "2025-08-14 18:02",
-    file: () => import("./second-post.md?raw"),
-    pinned: false,
-    lang: 'en'
-  },
-  {
-    id: 3,
-    img: Img,
-    hashtag: [
-      {
-        name: '#html',
-        color: ' text-orange-600',
-      },
-      {
-        name: '#css',
-        color: ' text-indigo-600',
-      },
-      {
-        name: '#react',
-        color: ' text-cyan-600',
-      },
-      {
-        name: '#angular',
-        color: ' text-red-600',
-      },
-    ],
-    name: "Kimdurdfbgsd",
-    slug: "xayr-oka",
-    date: "2025-08-14 18:02",
-    file: () => import("./second-post.md?raw"),
-    pinned: false,
-    lang: 'ru'
-  }
+  // {
+  //   id: 1,
+  //   img: Img,
+  //   hashtag: [
+  //     {
+  //       name: '#js',
+  //       color: 'text-yellow-600',
+  //     },
+  //     {
+  //       name: '#ts',
+  //       color: 'text-blue-700',
+  //     },
+  //     {
+  //       name: '#html',
+  //       color: 'text-orange-600',
+  //     },
+  //     {
+  //       name: '#css',
+  //       color: ' text-indigo-600',
+  //     }
+  //   ],
+  //   name: "Hello World",
+  //   slug: "hello-world",
+  //   date: "2025-08-14 18:02",
+  //   file: () => import("./hello-world.md?raw"),
+  //   pinned: false,
+  //   lang: 'uz'
+  // },
+  // {
+  //   id: 2,
+  //   img: Img,
+  //   hashtag: [
+  //     {
+  //       name: '#js',
+  //       color: ' text-yellow-600',
+  //     },
+  //     {
+  //       name: '#ts',
+  //       color: ' text-blue-600',
+  //     },
+  //     {
+  //       name: '#react',
+  //       color: ' text-cyan-600',
+  //     },
+  //     {
+  //       name: '#angular',
+  //       color: ' text-red-600',
+  //     },
+  //   ],
+  //   name: "Nimadur",
+  //   slug: "xayr-world",
+  //   date: "2025-08-14 18:02",
+  //   file: () => import("./second-post.md?raw"),
+  //   pinned: false,
+  //   lang: 'en'
+  // },
+  // {
+  //   id: 3,
+  //   img: Img,
+  //   hashtag: [
+  //     {
+  //       name: '#html',
+  //       color: ' text-orange-600',
+  //     },
+  //     {
+  //       name: '#css',
+  //       color: ' text-indigo-600',
+  //     },
+  //     {
+  //       name: '#react',
+  //       color: ' text-cyan-600',
+  //     },
+  //     {
+  //       name: '#angular',
+  //       color: ' text-red-600',
+  //     },
+  //   ],
+  //   name: "Kimdurdfbgsd",
+  //   slug: "xayr-oka",
+  //   date: "2025-08-14 18:02",
+  //   file: () => import("./second-post.md?raw"),
+  //   pinned: false,
+  //   lang: 'ru'
+  // }
 ]
 
 // text-yellow-600
